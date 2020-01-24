24ddc41726cc41b1e7a5648e701475e5425a2cc5
William Gonzalez <wifagoco@gmail.com>
2020-01-23 20:33:51 -0500

    Creacion de contexto de base de datos, siguiendo curso https://www.youtube.com/watch?v=hIQTCSulqx8&list=PLU3UD_RM_1Abkcw8jjCl4o179jfyD47mj&index=13

---
+++ DotNetCore2Coworking/Coworking.Api.DataAcces.Contracts/ICoworkingDBContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Coworking.Api.DataAcces.Contracts
+{
+    public interface ICoworkingDBContext
+    {
+    }
+}
---
+++ DotNetCore2Coworking/Coworking.Api.DataAcces/CoworkingDBContext.cs
@@ -0,0 +1,20 @@
+﻿using Coworking.Api.DataAcces.Contracts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Coworking.Api.DataAcces
+{
+    public class CoworkingDBContext : DbContext, ICoworkingDBContext
+    {
+        public CoworkingDBContext()
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) { 
+        
+        }
+    }
+}
